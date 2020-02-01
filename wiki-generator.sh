d941b7848f600337b9c587d3deef3230e1401a1d
wojtekg4455 <wojtekg4455@gmail.com>
2020-02-01 20:04:06 +0100

    change code

--- wiki-generator.sh
+++ wiki-generator.sh
@@ -3,7 +3,7 @@
 # Automatyczne generowanie WIKI dla Github'a
 
 
-echo "Wpisz wersję kernela np.: -5.5"
+echo "Wpisz wersję kernela np.: 5.5"
 read KERNEL
 
 echo "Wpisz nazwę pliku np.: wiki.txt"
@@ -25,15 +25,15 @@ ${CODE}usermod -G wheel USERNAME${CODE}
  ${CODE}gpg --locate-keys torvalds@kernel.org gregkh@kernel.org${CODE}
 ### 3. Verification of kernel sources :
 ### Download source and signature :
- ${CODE}wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux${KERNEL}.tar.xz${CODE}
+ ${CODE}wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL}.tar.xz${CODE}
 
- ${CODE}wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux${KERNEL}.tar.sign${CODE}
+ ${CODE}wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL}.tar.sign${CODE}
 ### Verify signature :
- ${CODE}unxz -c linux${KERNEL}.tar.xz | gpg --verify linux${KERNEL}.tar.sign -${CODE}
+ ${CODE}unxz -c linux-${KERNEL}.tar.xz | gpg --verify linux-${KERNEL}.tar.sign -${CODE}
 ### 4. Unpacking :
- ${CODE}tar xavf linux${KERNEL}.tar.xz${CODE}
+ ${CODE}tar xavf linux-${KERNEL}.tar.xz${CODE}
 ### 5. Copying the configuration :
- ${CODE}cp config-arch-default linux${KERNLE}/.config${CODE}
+ ${CODE}cp config-arch-default linux-${KERNLE}/.config${CODE}
 ### Optionally, you can use :
  ${CODE}make localmodconfig${CODE}
 ### 6. Own settings :
@@ -43,28 +43,28 @@ ${CODE}usermod -G wheel USERNAME${CODE}
 ### 8. Install the modules :
  ${CODE}sudo make modules_install${CODE}
 ### 9. Copying the new kernel to the / boot folder :
- ${CODE}sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-linux${KERNEL}${CODE}
+ ${CODE}sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-linux-${KERNEL}${CODE}
 ### 10. Make initial RAM disk :
- ${CODE}sudo cp /etc/mkinitcpio.d/linux.preset /etc/mkinitcpio.d/linux${KERNEL}.preset${CODE}
-### 11. Edit files linux${KERNEL}.preset
- ${CODE}sudo vim /etc/mkinitcpio.d/linux${KERNEL}.preset${CODE}
+ ${CODE}sudo cp /etc/mkinitcpio.d/linux.preset /etc/mkinitcpio.d/linux-${KERNEL}.preset${CODE}
+### 11. Edit files linux-${KERNEL}.preset
+ ${CODE}sudo vim /etc/mkinitcpio.d/linux-${KERNEL}.preset${CODE}
 ***
 
  ${CODE}ALL_config="/etc/mkinitcpio.conf${CODE}
 
- ${CODE}ALL_kver="/boot/vmlinuz-linux${KERNEL}${CODE}
+ ${CODE}ALL_kver="/boot/vmlinuz-linux-${KERNEL}${CODE}
 
  ${CODE}PRESETS=('default' 'fallback')${CODE}
 
- ${CODE}default_image="/boot/initramfs-linux${KERNEL}.img${CODE}
+ ${CODE}default_image="/boot/initramfs-linux-${KERNEL}.img${CODE}
 
- ${CODE}fallback_image="/boot/initramfs-linux${KERNEL}-fallback.img${CODE}
+ ${CODE}fallback_image="/boot/initramfs-linux-${KERNEL}-fallback.img${CODE}
 
  ${CODE}fallback_options="-S autodetect${CODE}
 
 **We issue the command :**
 
- ${CODE}sudo mkinitcpio -p linux${KERNEL}${CODE}
+ ${CODE}sudo mkinitcpio -p linux-${KERNEL}${CODE}
 
 ### 12. Refresh the THICK configuration :
  ${CODE}sudo grub-mkconfig -o /boot/grub/grub.cfg${CODE}
