70c297aa87f03b04f839d4e075b5a4b3911b8f3d
wojtekg4455 <wojtekg4455@gmail.com>
2020-02-01 21:07:49 +0100

    add files

---
+++ kompilator.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+# BY WOJTEK 2020
+# Automatyczna kompilacja kernela
+
+echo "Podaj nową wersję kernela np.: 5.5.1"
+read KERNEL
+
+echo "Podaj starą wersję kernela np. 5.5"
+read SKERNEL
+
+unxz -c linux-${KERNEL}.tar.xz | gpg --verify linux-${KERNEL}.tar.sign -
+
+tar xavf linux-${KERNEL}.tar.xz
+
+cp linux-${SKERNEL}/.config linux-${KERNEL}/.config
+
+cd linux-${KERNEL}
+
+make menuconfig
+
+make -j 16
+
+sudo make modules_install
+
+sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-linux-${KERNEL}
+
+
