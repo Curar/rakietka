f98a6cabe8127be70485066629000189621f0777
wojtekg4455 <wojtekg4455@gmail.com>
2020-02-11 22:28:15 +0100

    Zmiana wget na curl git add .!

--- kompilator-1.3.sh
+++ kompilator-1.3.sh
@@ -26,8 +26,8 @@ ADRES_PODPISU="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL}.tar.
 function download {
         if [ ! -e "$KERNEL_EXIST" ] && [ ! -e "$KERNEL_SIGN" ]
 	then
-	curl -o "$KERNEL_EXIST" "$ADRES_KERNELA" 2>&1 | dialog --progressbox 10 100 --gauge "Pobieram : ${KERNEL_EXIST}" 10 100
-	curl -o "$KERNEL_SIGN" "$ADRES_PODPISU" 2>&1 | dialog --progressbox 10 100 --gauge "Pobieram: ${KERNEL_SIGN}" 10 100
+	curl -o "$KERNEL_EXIST" "$ADRES_KERNELA" 2>&1 | dialog --progressbox 10 100
+	curl -o "$KERNEL_SIGN" "$ADRES_PODPISU" 2>&1 | dialog --progressbox 10 100
 	clear	
 	else
 	echo -e "\e[32m===========================\e[0m"
